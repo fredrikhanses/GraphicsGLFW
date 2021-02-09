e504f7e6e1aa1ab982ca654c90ba7eab29d864f1
Fredrik Hanses <Fredrik.Hanses@futuregames.nu>
2021-02-09 17:25:13 +0100

    Added GLEW. Video 7 done

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define GLEW_STATIC
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <GL/GL.h>
 
 void handleKeyEvent(GLFWwindow* window, int key, int scancode, int action, int modifiers)
 {
@@ -70,13 +73,31 @@ void handleMouseEvent(GLFWwindow* window, int button, int action, int modifiers)
 int main ()
 {
 	glfwInit();
-	GLFWwindow* window = glfwCreateWindow(600, 800, "GraphicsGLFW", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "GraphicsGLFW", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	glewInit();
 	glfwSetKeyCallback(window, handleKeyEvent);
 	glfwSetMouseButtonCallback(window, handleMouseEvent);
+	//VBO
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	float vertexData[] = { 0.0f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f };
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+	//VAO
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, 0);
+
 	while (!glfwWindowShouldClose(window))
 	{
+		glClearColor(1.0f, 0.5f, 0.25f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glfwSwapBuffers(window);
 		glfwPollEvents();
-		
 	}
 	return 0;
 }

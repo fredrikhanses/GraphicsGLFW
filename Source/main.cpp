c33bc3d4605edec43fa69c94542bece54b4980b7
fredrikhanses <fredrik.hanses@gmail.com>
2021-02-10 18:01:22 +0100

    Third assignment done. Changed to use element buffer

--- Source/main.cpp
+++ Source/main.cpp
@@ -41,13 +41,13 @@ GLuint createShaderProgram(const char* vertexSource, const char* fragmentSource)
 	return program;
 }
 
-GLuint createVertexArrayBuffer(float* vertexData, int dataSize)
+GLuint createVertexArrayBuffer(float* vertexData, int vertexDataSize, GLuint* elementData = nullptr, int elemetDataSize = 0)
 {
 	//Vertex Buffer Object
 	GLuint vertexBufferObject;
 	glGenBuffers(1, &vertexBufferObject);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
-	glBufferData(GL_ARRAY_BUFFER, dataSize, vertexData, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, vertexDataSize, vertexData, GL_STATIC_DRAW);
 
 	//Vertex Array Object
 	GLuint vertexArrayObject;
@@ -55,6 +55,15 @@ GLuint createVertexArrayBuffer(float* vertexData, int dataSize)
 	glBindVertexArray(vertexArrayObject);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, 0);
+
+	if (elementData != nullptr && elemetDataSize != 0)
+	{
+		//Element Buffer Object
+		GLuint elementBufferObject;
+		glGenBuffers(1, &elementBufferObject);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferObject);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, elemetDataSize, elementData, GL_STATIC_DRAW);
+	}
 
 	return vertexArrayObject;
 }
@@ -130,13 +139,19 @@ int main ()
 	TextReader textReader;
 	std::string vertexShaderSource = textReader.ReadText("Shaders/VertexShader.txt");
 	char* vertexShaderSourceChar = &vertexShaderSource[0];
-	std::string fragmentShaderSource = textReader.ReadText("Shaders/FragmentShader.txt");
-	char* fragmentShaderSourceChar = &fragmentShaderSource[0];
+	std::string fragmentShaderSourceA = textReader.ReadText("Shaders/FragmentShaderA.txt");
+	char* fragmentShaderSourceCharA = &fragmentShaderSourceA[0];
 	std::string fragmentShaderSourceB = textReader.ReadText("Shaders/FragmentShaderB.txt");
 	char* fragmentShaderSourceCharB = &fragmentShaderSourceB[0];
+	std::string fragmentShaderSourceC = textReader.ReadText("Shaders/FragmentShaderC.txt");
+	char* fragmentShaderSourceCharC = &fragmentShaderSourceC[0];
+	std::string fragmentShaderSourceD = textReader.ReadText("Shaders/FragmentShaderD.txt");
+	char* fragmentShaderSourceCharD = &fragmentShaderSourceD[0];
+	std::string fragmentShaderSourceE = textReader.ReadText("Shaders/FragmentShaderE.txt");
+	char* fragmentShaderSourceCharE = &fragmentShaderSourceE[0];
 
 	glfwInit();
-	GLFWwindow* window = glfwCreateWindow(800, 600, "GraphicsGLFW", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 800, "GraphicsGLFW", NULL, NULL);
 	glfwMakeContextCurrent(window);
 
 	glewInit();
@@ -145,28 +160,101 @@ int main ()
 	glfwSetMouseButtonCallback(window, handleMouseEvent);
 
 	//Program A
-	float vertexDataA[] = { 0.0f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f };
-	GLuint triA = createVertexArrayBuffer(vertexDataA, sizeof(vertexDataA));
-	GLuint programA = createShaderProgram(vertexShaderSourceChar, fragmentShaderSourceChar);
+	float vertexDataA[] =
+	{ 
+		-0.9, 0.7, 
+		-0.9f, 0.9f, 
+		-0.1f, 0.9f, 
+		-0.1f, 0.7f 
+	};
+	GLuint elementDataA[] = { 0, 1, 2, 0, 2, 3 };
+	GLuint triA = createVertexArrayBuffer(vertexDataA, sizeof(vertexDataA), elementDataA, sizeof(elementDataA));
+	GLuint programA = createShaderProgram(vertexShaderSourceChar, fragmentShaderSourceCharA);
 
 	//Program B
-	float vertexDataB[] = { 0.4f, 1.0f, 0.7f, -0.3f, 0.9f, 0.5f };
-	GLuint triB = createVertexArrayBuffer(vertexDataB, sizeof(vertexDataB));
+	float vertexDataB[] = 
+	{ 
+		0.5f, 0.5f, 
+		0.5f, 0.9f, 
+		0.9f, 0.9f, 
+		0.9f, 0.5f 
+	};
+	GLuint elementDataB[] = { 0, 1, 2, 0, 2, 3 };
+	GLuint triB = createVertexArrayBuffer(vertexDataB, sizeof(vertexDataB), elementDataB, sizeof(elementDataB));
 	GLuint programB = createShaderProgram(vertexShaderSourceChar, fragmentShaderSourceCharB);
+
+	//Program C
+	float vertexDataC[] =
+	{ 
+		-0.9f, -0.4f, 
+		-0.9f, 0.2f, 
+		-0.7f, 0.2f, 
+		-0.7f, -0.4f, 
+		-0.7f, -0.2f, 
+		-0.5f, -0.2f, 
+		-0.5f, -0.4f 
+	};
+	GLuint elementDataC[] = { 0, 1, 2, 0, 2, 3, 3, 4, 5, 3, 5, 6 };
+	GLuint triC = createVertexArrayBuffer(vertexDataC, sizeof(vertexDataC), elementDataC, sizeof(elementDataC));
+	GLuint programC = createShaderProgram(vertexShaderSourceChar, fragmentShaderSourceCharC);
+
+	//Program D
+	float vertexDataD[] =
+	{ 
+		0.2f, -0.4f,
+		0.2f, 0.0f,
+		0.4f, 0.0f,
+		0.4f, -0.4f,
+		0.4f, -0.2f,
+		0.6f, -0.2f,
+		0.6f, -0.6f,
+		0.4f, -0.6f 
+	};
+	GLuint elementDataD[] = { 0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7 };
+	GLuint triD = createVertexArrayBuffer(vertexDataD, sizeof(vertexDataD), elementDataD, sizeof(elementDataD));
+	GLuint programD = createShaderProgram(vertexShaderSourceChar, fragmentShaderSourceCharD);
+
+	//Program E
+	float vertexDataE[] = 
+	{ 
+		-0.1f, 0.0f,
+		-0.3f, 0.0f,
+		-0.3f, 0.2f,
+		-0.1f, 0.2f,
+		-0.5f, 0.2f,
+		-0.5f, 0.4f,
+		0.1f, 0.4f,
+		0.1f, 0.2f
+	};
+	GLuint elementDataE[] = { 0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7 };
+	GLuint triE = createVertexArrayBuffer(vertexDataE, sizeof(vertexDataE), elementDataE, sizeof(elementDataE));
+	GLuint programE = createShaderProgram(vertexShaderSourceChar, fragmentShaderSourceCharE);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		//Clear screen
-		glClearColor(1.0f, 0.5f, 0.25f, 1.0f);
+		glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(programA);
 		glBindVertexArray(triA);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glUseProgram(programB);
 		glBindVertexArray(triB);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glUseProgram(programC);
+		glBindVertexArray(triC);
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+
+		glUseProgram(programD);
+		glBindVertexArray(triD);
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+
+		glUseProgram(programE);
+		glBindVertexArray(triE);
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
